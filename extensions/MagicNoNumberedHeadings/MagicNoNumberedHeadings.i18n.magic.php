6876cc51547fffb754d91ac92c310c09723d47ec
Phanoix <phanoix@gmail.com>
2016-01-18 14:56:32 -0500

    upgrading gcpedia...

---
+++ extensions/MagicNoNumberedHeadings/MagicNoNumberedHeadings.i18n.magic.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Internationalisation file for MagicNoNumberedHeadings extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$magicWords = array();
+
+/** English (English) */
+$magicWords['en'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__NONUMBEREDHEADINGS__' ),
+);
+
+/** Arabic (العربية) */
+$magicWords['ar'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, 'لا_عناوين_مرقمة' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$magicWords['arz'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__لا_عناوين_مرقمة__', 'لا_عناوين_مرقمة', '__NONUMBEREDHEADINGS__' ),
+);
+
+/** Chechen (нохчийн) */
+$magicWords['ce'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__ТЕРАХЬДОЦУШКОРТА__', '__БЕЗНОМЕРОВЗАГОЛОВКОВ__', '__NONUMBEREDHEADINGS__' ),
+);
+
+/** German (Deutsch) */
+$magicWords['de'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__KEINE_ÜBERSCHRIFTENNUMMERIERUNG__' ),
+);
+
+/** Zazaki (Zazaki) */
+$magicWords['diq'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__SERNAMEYONIMREYINÇINO__' ),
+);
+
+/** Spanish (español) */
+$magicWords['es'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__ENCABEZADOSNOENUMERADOS__', '__NONUMERARENCABEZADOS__' ),
+);
+
+/** Korean (한국어) */
+$magicWords['ko'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__번호매긴제목숨김__', '__번호매긴표제숨김__' ),
+);
+
+/** Colognian (Ripoarisch) */
+$magicWords['ksh'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__ÖVERSCHRIFTENITNUMMERIERE__', '__KEINEÜERSCHRIFTENNUMMERIERUNG__', '__NONUMBEREDHEADINGS__' ),
+);
+
+/** Macedonian (македонски) */
+$magicWords['mk'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__БЕЗБРОЕВИВОЗАГЛАВИЈАТА__' ),
+);
+
+/** Malayalam (മലയാളം) */
+$magicWords['ml'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__ക്രമസംഖ്യാരഹിതതലക്കെട്ടുകൾ__' ),
+);
+
+/** Marathi (मराठी) */
+$magicWords['mr'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__विनाअंकमथळे__', '__NONUMBEREDHEADINGS__' ),
+);
+
+/** Nedersaksisch (Nedersaksisch) */
+$magicWords['nds-nl'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__GIENENUMMERDENKOPJES__', '__GEENGENUMMERDEKOPPEN__' ),
+);
+
+/** Dutch (Nederlands) */
+$magicWords['nl'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__GEENGENUMMERDEKOPPEN__' ),
+);
+
+/** Polish (polski) */
+$magicWords['pl'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__NIENUMERUJNAGŁÓWKÓW__' ),
+);
+
+/** Russian (русский) */
+$magicWords['ru'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__БЕЗНОМЕРОВЗАГОЛОВКОВ__' ),
+);
+
+/** Serbian (Latin script) (srpski (latinica)‎) */
+$magicWords['sr-el'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__BEZNUMERISANIHNASLOVA__', '__BEZ_NUMERISANIH_NASLOVA__' ),
+);
+
+/** Swedish (svenska) */
+$magicWords['sv'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__INTENUMRERADERUBRIKER__', '__NONUMBEREDHEADINGS__' ),
+);
+
+/** Turkish (Türkçe) */
+$magicWords['tr'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__NUMARALIBAŞLIKYOK__' ),
+);
+
+/** Ukrainian (українська) */
+$magicWords['uk'] = array(
+	'MAG_NONUMBEREDHEADINGS' => array( 0, '__БЕЗНОМЕРІВЗАГОЛОВКІВ__', '__БЕЗНОМЕРОВЗАГОЛОВКОВ__' ),
+);
---
+++ extensions/MagicNumberedHeadings/MagicNumberedHeadings.i18n.php
@@ -0,0 +1,17 @@
+<?php
+
+$messages = array();
+
+$magicWords = array();
+
+$magicWords['en'] = array(
+        'MAG_NUMBEREDHEADINGS' => array( 0, '__NUMBEREDHEADINGS__' ),
+);
+
+$magicWords['de'] = array(
+        'MAG_NUMBEREDHEADINGS' => array( 0, '__ÜBERSCHRIFTENNUMMERIERUNG__' ),
+);
+
+$magicWords['ksh'] = array(
+        'MAG_NUMBEREDHEADINGS' => array( 0, '__ÖVVERSCHRIFTENUMMERIERE__' ),
+);
