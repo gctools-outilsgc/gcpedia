d83ab2ac1532cf59c2162929ee8b4759318e52ff
Phanoix <phanoix@gmail.com>
2016-02-11 11:05:07 -0500

    Collection extension

---
+++ extensions/Collection/Gruntfile.js
@@ -0,0 +1,29 @@
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'modules/*.js'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'jshint', 'jscs', 'jsonlint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
