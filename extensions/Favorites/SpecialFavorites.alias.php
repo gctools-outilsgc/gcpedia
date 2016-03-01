0e75993267c4ee5f5d8c6a2591e41858204b9fd9
Phanoix <phanoix@gmail.com>
2016-03-01 12:21:15 -0500

    Favorites and Replace Text extensions.

---
+++ extensions/Favorites/SpecialFavorites.alias.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Aliases for Favorites
+ *
+ * @file
+ * @ingroup Extensions
+ */
+ 
+$specialPageAliases = array();
+ 
+/** English
+ * @author Jeremy Lemley
+ */
+$specialPageAliases['en'] = array(
+	'Favoritelist' => array( 'Favoritelist', 'My Favorites' ),
+);
+ 
