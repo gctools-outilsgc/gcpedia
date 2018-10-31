7f39853179ccc818453bc56ed6eea81d8c19b094
Phanoix <phanoix@gmail.com>
2018-10-31 16:32:17 -0400

    merge PR branch with master for image build

--- brigade.js
+++ brigade.js
@@ -48,12 +48,15 @@ events.on("pull_request", function(e, project) {
   var build = new Job("build", "docker:dind")
   build.privileged = true;
   build.env.TAG = `${ JSON.parse(e.payload).number }`
+  build.env.BRANCH = `${ JSON.parse(e.payload).head.ref }`
   build.env.DOCKER_USER = project.secrets.dockerUsr
   build.env.DOCKER_PASS = project.secrets.dockerPass
   build.tasks = [
     "dockerd-entrypoint.sh &", // Start the docker daemon
     "sleep 20", // Grant it enough time to be up and running
     "cd /src/",
+    "git checkout master",
+    "git merge --no-ff $BRANCH",
     "docker build -t phanoix/gcpedia:pr-$TAG .",
     "docker login -u $DOCKER_USER -p $DOCKER_PASS",
     "docker push phanoix/gcpedia:pr-$TAG"
@@ -69,14 +72,15 @@ events.on("pull_request", function(e, project) {
   // notify via Rocket.Chat webhook
   var notify = new Job("notify", "alpine:3.4")
   notify.env.CHATKEY = project.secrets.chatKey
+  notify.env.PRNUM = `${ JSON.parse(e.payload).number }`
   notify.tasks = [
     "apk update",
     "apk add curl",
-    "curl -X POST -H 'Content-Type: application/json' --data '{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Wiki image built, dev updating.\",\"attachments\":[{\"title\":\"Brigade script finished!\",\"title_link\": \"https://hub.docker.com/r/phanoix/gcpedia/tags/\",\"text\": \"The new wiki image is available at Docker hub.\",\"color\":\"#764FA5\"}]}' https://message.gccollab.ca/hooks/$CHATKEY"      //test rocket chat notification
+    "curl -X POST -H 'Content-Type: application/json' --data `{\"username\":\"Brigade\",\"icon_emoji\":\":k8s:\",\"text\":\"Wiki image built, PR ready for testing.\",\"attachments\":[{\"title\":\"Brigade script finished!\",\"title_link\": \"https://github.com/gctools-outilsgc/gcpedia/pull/$PRNUM\",\"text\": \"The new wiki image is available at Docker hub.\",\"color\":\"#764FA5\"}]}` https://message.gccollab.ca/hooks/$CHATKEY"      //test rocket chat notification
   ]
   
   pending.run().then(() => { build.run().then(() => { update.run().then(() => { notify.run().then(() => { 
-    return ghNotify("success", `Build ${ e.buildID } succeded, test image ready`, e, project).run() }) }) }) })
+    return ghNotify("success", `Build for PR ${ JSON.parse(e.payload).number } succeded, test image ready`, e, project).run() }) }) }) })
 })
 
 
