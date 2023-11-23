9b5877393e85f2b327695f599dc01434d59fc3d3
phanoix <phanoix@gmail.com>
2023-11-23 13:49:06 -0500

    env toggle and updated init for gcpedia and wiki

--- docker/start.sh
+++ docker/start.sh
@@ -8,6 +8,10 @@ then
   
   echo "install complete, swapping in env-based LocalSettings.php"
   cp /var/www/html/docker/LocalSettings.php.docker /var/www/html/LocalSettings.php
+
+  # run script with the full config
+  echo "running update maintenance script"
+  php /var/www/html/maintenance/update.php -q
 fi
 
 # Start server - depending on the image, one of these will work
