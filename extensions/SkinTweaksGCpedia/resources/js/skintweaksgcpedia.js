7f71c4d62cbe5e37a3a641fda421bc164265ed93
phanoix <phanoix@gmail.com>
2024-01-22 09:40:55 -0500

    fix for the gcpedia header language toggle

--- extensions/SkinTweaksGCpedia/resources/js/skintweaksgcpedia.js
+++ extensions/SkinTweaksGCpedia/resources/js/skintweaksgcpedia.js
@@ -80,6 +80,13 @@ function addTopBar(){
 </div>";
     
     document.body.insertBefore(header, document.body.firstChild);
+
+
+    $('#header-lang-toggle').on( 'click', (Event) =>
+        mw.loader.using(['ext.uls.common']).then(function() {
+            mw.uls.changeLanguage( Event.target.lang );
+        })
+    );
 }
 
 addTopBar();
