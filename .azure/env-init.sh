2b7f55fe7c5167ba616e06b7f2fb7d15703cd8aa
Ilia <phanoix@gmail.com>
2024-02-13 14:50:23 -0500

    Update env-init.sh

--- .azure/env-init.sh
+++ .azure/env-init.sh
@@ -4,5 +4,6 @@ apt-get update && apt install -y --no-install-recommends memcached imagemagick l
 cp /home/site/wwwroot/gcwiki/.azure/nginx.config /etc/nginx/sites-enabled/default
 service memcached start
 service nginx reload
+service memcached restart
 
 # any php extensions need to be installed or activated?
