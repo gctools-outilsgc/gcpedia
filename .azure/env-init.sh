6e2150c7b1c2f509bd3689bc4ba0b00ba604d568
phanoix <phanoix@gmail.com>
2024-02-13 16:31:36 -0500

    memcached should properly start like this

--- .azure/env-init.sh
+++ .azure/env-init.sh
@@ -3,8 +3,5 @@
 apt-get update && apt install -y --no-install-recommends memcached imagemagick libicu-dev libonig-dev htmldoc ffmpeg
 cp /home/site/wwwroot/gcwiki/.azure/nginx.config /etc/nginx/sites-enabled/default
 service nginx reload
-sleep 3
-service memcached start
-service memcached status
 
 # any php extensions need to be installed or activated?
