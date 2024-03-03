167e28d02c621154fc007a762265606d615a662f
vid <vid@users.noreply.github.com>
2024-03-03 00:17:10 +0000

    update e2e test reviews

--- haibun/reviews/build/dashboard/indexer.js
+++ haibun/reviews/build/dashboard/indexer.js
@@ -1,2 +1,2 @@
 export const endpoint = "./tracks/";
-export async function getPublishedReviews() { return ["1709410736516-tracksHistory.json"]; }
+export async function getPublishedReviews() { return ["1707419810394-tracksHistory.json","1707420169226-tracksHistory.json","1709425022800-tracksHistory.json"]; }
