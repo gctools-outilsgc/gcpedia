02398d0da1e70f646799bf09b6e5d695acc7863f
vid <vid_github@zooid.org>
2024-06-03 15:39:47 -0400

    normalize formatting

--- site/config-gcwiki.php
+++ site/config-gcwiki.php
@@ -2,35 +2,35 @@
 
 $wgArticlePath = "/$1";
 
-wfLoadSkin( 'Vector' );
+wfLoadSkin('Vector');
 $wgLocaltimezone = "America/Montreal";
 
 $wgLogo = '';
 $wgFavicon = "extensions/SkinTweaksGCwiki/resources/images/mini_wiki_icon.png";
-$wgLogos = [ 
-    '1x' =>  'extensions/SkinTweaksGCwiki/resources/images/collab_logo_en_1x.png',
+$wgLogos = [
+    '1x' => 'extensions/SkinTweaksGCwiki/resources/images/collab_logo_en_1x.png',
     'variants' => [
-		'fr' => [
+        'fr' => [
             '1x' => "extensions/SkinTweaksGCwiki/resources/images/collab_logo_fr_1x.png",
         ],
     ],
 ];
 
 $wgNamespacesWithSubpages[NS_MAIN] = true;
-  
+
 # Disable EDIT for 'everyone'
-$wgGroupPermissions['*']['edit']              = false;
+$wgGroupPermissions['*']['edit'] = false;
 
 # Disable for users, too: by default 'user' is allowed to edit, even if '*' is not.
 //REGULAR USER PERMISSIONS
-$wgGroupPermissions['user']['edit']           = true;
-$wgGroupPermissions['user']['move']           = true;
-$wgGroupPermissions['user']['delete']         = false;
-$wgGroupPermissions['user']['undelete']       = true;
+$wgGroupPermissions['user']['edit'] = true;
+$wgGroupPermissions['user']['move'] = true;
+$wgGroupPermissions['user']['delete'] = false;
+$wgGroupPermissions['user']['undelete'] = true;
 $wgGroupPermissions['user']['deletedhistory'] = true;
 
 $wgEnableUploads = true;
-$wgFileExtensions = array('pub','png','gif','jpg','jpeg','pdf','xls','docx','pptx','doc','ppt','svg','xml','mpg','odp','odt','wmv','flv','vsd','mpp','ai','zip','txt','wpd','rtf','drf','xlsx','xlsm', 'oft');
+$wgFileExtensions = array('pub', 'png', 'gif', 'jpg', 'jpeg', 'pdf', 'xls', 'docx', 'pptx', 'doc', 'ppt', 'svg', 'xml', 'mpg', 'odp', 'odt', 'wmv', 'flv', 'vsd', 'mpp', 'ai', 'zip', 'txt', 'wpd', 'rtf', 'drf', 'xlsx', 'xlsm', 'oft');
 
 $wgUseAjax = true;
 $wgEnableAPI = true;
@@ -46,45 +46,45 @@ $wgRightsText = "Creative Commons Attribution 4.0 International License";
 $wgRightsIcon = "https://i.creativecommons.org/l/by/4.0/88x31.png";
 
 ////  extensions
-wfLoadExtension( "ParserFunctions" );
+wfLoadExtension("ParserFunctions");
 
 require_once "$IP/extensions/MagicNoNumberedHeadings/MagicNoNumberedHeadings.php";
 require_once "$IP/extensions/MagicNumberedHeadings/MagicNumberedHeadings.php";
 
-wfLoadExtension( "UniversalLanguageSelector" );
+wfLoadExtension("UniversalLanguageSelector");
 $wgULSGeoService = false;
 
-wfLoadExtension( "WikiEditor" );
+wfLoadExtension("WikiEditor");
 # Enable WikiEditor toolbar as default (but still allow users to disable)
 $wgDefaultUserOptions["usebetatoolbar"] = 1;
 $wgDefaultUserOptions["usebetatoolbar-cgd"] = 0;
 $wgDefaultUserOptions["wikieditor-preview"] = 0;
 
-wfLoadExtension( "Cite" );
+wfLoadExtension("Cite");
 
-wfLoadExtension( 'DeletePagesForGood' );
-$wgGroupPermissions['*']         ['deleteperm'] = false;
-$wgGroupPermissions['user']      ['deleteperm'] = false;
+wfLoadExtension('DeletePagesForGood');
+$wgGroupPermissions['*']['deleteperm'] = false;
+$wgGroupPermissions['user']['deleteperm'] = false;
 $wgGroupPermissions['bureaucrat']['deleteperm'] = true;
-$wgGroupPermissions['sysop']     ['deleteperm'] = false;
-$wgGroupPermissions['IMadmin']   ['deleteperm'] = true;
-$wgGroupPermissions['IMadmin']   ['delete'] = true;
-$wgGroupPermissions['IMadmin']   ['undelete'] = true;
+$wgGroupPermissions['sysop']['deleteperm'] = false;
+$wgGroupPermissions['IMadmin']['deleteperm'] = true;
+$wgGroupPermissions['IMadmin']['delete'] = true;
+$wgGroupPermissions['IMadmin']['undelete'] = true;
 
-wfLoadExtension( "CharInsert" );
-wfLoadExtension( "TreeAndMenu" );
-wfLoadExtension( "InputBox" );
-wfLoadExtension( "Gadgets" );
-wfLoadExtension( "ImageMap" );
+wfLoadExtension("CharInsert");
+wfLoadExtension("TreeAndMenu");
+wfLoadExtension("InputBox");
+wfLoadExtension("Gadgets");
+wfLoadExtension("ImageMap");
 $wgImageMapAllowExternalLinks = false;    // allowing this seems like a bad idea
-wfLoadExtension( "InputBox" );
-wfLoadExtension( "Lingo" );
-wfLoadExtension( "VisualEditor" );
+wfLoadExtension("InputBox");
+wfLoadExtension("Lingo");
+wfLoadExtension("VisualEditor");
 
 // pdf rendering service
 #wfLoadExtension("ElectronPdfService");
 #$wgElectronPdfServiceRESTbaseURL = 'https://pdf.gccollab.ca/pdf?accessKey=secret&url=https://wiki.gccollab.ca/';
-wfLoadExtension( 'PdfBook' );
+wfLoadExtension('PdfBook');
 $wgPdfBookTab = false;
 $wgPdfBookDownload = false;
 
@@ -98,29 +98,29 @@ $wgDLPAllowUnlimitedCategories = false; // Allow unlimited categories
 // How long to cache pages using DPL's in seconds. Default to 1 day. Set to
 // false to use the normal amount of page caching (most efficient), Set to 0 to disable
 // cache altogether (inefficient, but results will never be outdated)
-$wgDLPMaxCacheTime = 60*60*24;          // How long to cache pages in seconds
+$wgDLPMaxCacheTime = 60 * 60 * 24;          // How long to cache pages in seconds
 
-wfLoadExtension( "LookupUser" );
+wfLoadExtension("LookupUser");
 $wgGroupPermissions['sysop']['lookupuser'] = true;
 
-wfLoadExtension( "CategoryTree" );
+wfLoadExtension("CategoryTree");
 wfLoadExtension("CSS");
 wfLoadExtension("EditUser");
 $wgGroupPermissions['bureaucrat']['edituser'] = true;
 $wgGroupPermissions['sysop']['edituser-exempt'] = true;
 
-wfLoadExtension( 'CategoryWatch' );
+wfLoadExtension('CategoryWatch');
 
 wfLoadExtension("RSS");
 $wgRSSUrlWhitelist = array("*");
 $wgRSSUrlNumberOfAllowedRedirects = 1;
 
-require_once( "$IP/extensions/Multilang/multilang.php" );
+require_once ("$IP/extensions/Multilang/multilang.php");
 
 wfLoadExtension("MobileFrontend");
 $wgMFAutodetectMobileView = true;
 
-require_once("$IP/extensions/GC_Messages/GC_Messages.php");
+require_once ("$IP/extensions/GC_Messages/GC_Messages.php");
 
 # Add a "My Favorites" link to the personal URLs area:
 #wfLoadExtension("Favorites");
@@ -128,24 +128,24 @@ require_once("$IP/extensions/GC_Messages/GC_Messages.php");
 # Add a Star icon for selecting favorites:
 #$wgUseIconFavorite = false;
 
-wfLoadExtension( "ReplaceText" );
+wfLoadExtension("ReplaceText");
 $wgGroupPermissions['bureaucrat']['replacetext'] = true;
 
-wfLoadExtension( "UserMerge" );
+wfLoadExtension("UserMerge");
 $wgGroupPermissions['sysop']['usermerge'] = true;
-$wgUserMergeUnmergeable = array( 'sysop', 'awesomeusers' );
- 
+$wgUserMergeUnmergeable = array('sysop', 'awesomeusers');
+
 #$wgShowSQLErrors = true;
 #$wgShowExceptionDetails = true;
 
-wfLoadExtension( "IframePage" );
-$wgIframePageSrc= array( 'GCcollab' => 'https://gccollab.ca/', 'YouTube' => 'https://www.youtube.com/embed/' );
+wfLoadExtension("IframePage");
+$wgIframePageSrc = array('GCcollab' => 'https://gccollab.ca/', 'YouTube' => 'https://www.youtube.com/embed/');
 $wgIframePageAllowPath = true;
 
-wfLoadExtension( "Nuke" );
+wfLoadExtension("Nuke");
 $wgGroupPermissions['sysop']['nuke'] = true;
 
-wfLoadExtension( "SkinTweaksGCwiki" );
+wfLoadExtension("SkinTweaksGCwiki");
 
 require_once "$IP/extensions/googleAnalytics/googleAnalytics.php";
 $wgGoogleAnalyticsAccount = $GAaccount;
